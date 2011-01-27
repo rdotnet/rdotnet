293a1747bbc69d3a4b2814036888f1771f88465c
Kosei ABE <mail@recyclebin.jp>
2011-01-28 04:52:08 +0900

    Modified ParseException as accessible to the statement causing an error.

--- R.NET/ParseException.cs
+++ R.NET/ParseException.cs
@@ -16,28 +16,44 @@ namespace RDotNet
 			}
 		}
 
+		private const string ErrorStatementFieldName = "errorStatement";
+		private readonly string errorStatement;
+		public string ErrorStatement
+		{
+			get
+			{
+				return errorStatement;
+			}
+		}
+
+
 		public ParseException()
 			: base()
 		{
 			// This does not internally occur. See Parse.h in R_HOME/include/R_ext/Parse.h
 			this.status = ParseStatus.Null;
+			this.errorStatement = null;
 		}
 
-		public ParseException(ParseStatus status)
+		public ParseException(ParseStatus status, string errorStatement)
+			: base()
 		{
 			this.status = status;
+			this.errorStatement = errorStatement;
 		}
 
 		protected ParseException(SerializationInfo info, StreamingContext context)
 			: base(info, context)
 		{
 			this.status = (ParseStatus)info.GetValue(StatusFieldName, typeof(ParseStatus));
+			this.errorStatement = info.GetString(ErrorStatementFieldName);
 		}
 
 		public override void GetObjectData(SerializationInfo info, StreamingContext context)
 		{
 			base.GetObjectData(info, context);
 			info.AddValue(StatusFieldName, status);
+			info.AddValue(ErrorStatementFieldName, errorStatement);
 		}
 	}
 }
