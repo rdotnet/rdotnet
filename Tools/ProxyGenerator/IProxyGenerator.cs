ce99d19d3154cc53e114d4278997336604fe678b
kos59125 <kos59125@localhost>
2011-02-11 14:44:24 +0900

    Created a toy of generating proxy classes.

---
+++ Tools/ProxyGenerator/IProxyGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.CodeDom;
+
+namespace RDotNet.Tools
+{
+	public interface IProxyGenerator
+	{
+		CodeCompileUnit GenerateProxy(string proxyTypeName, Type proxyInterface);
+	}
+}
