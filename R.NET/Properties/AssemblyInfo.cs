82f5c7e39a409af6631535e4aed15a888b6976db
Kosei ABE <mail@recyclebin.jp>
2013-08-10 22:26:02 +0900

    Fix assembly versions.

--- R.NET/Properties/AssemblyInfo.cs
+++ R.NET/Properties/AssemblyInfo.cs
@@ -35,6 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.5.2.*")]
-[assembly: AssemblyFileVersion("1.5.2.*")]
+[assembly: AssemblyVersion("1.5.2.0")]
 [assembly: AssemblyInformationalVersion("1.5.2")]
