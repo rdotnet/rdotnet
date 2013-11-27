f40159f3550b6cb66285aa097aa2c05ac058a2d0
J-M <jean-michel.perraud@csiro.au>
2013-11-28 09:03:15 +1100

    Unit test to cover workitem 70, making sure that .NET exceptions are not caught prematurely by R on *nix platforms

---
+++ RDotNet.Tests/ExceptionHandlingTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RDotNet
+{
+   public class ExceptionHandlingTest : RDotNetTestFixture
+   {
+      [Test, ExpectedException(typeof(NullReferenceException))]
+      public void TestCharacter()
+      {
+         // Check that https://rdotnet.codeplex.com/workitem/70 does not occur; in particular worth testing on CentOS according to issue reporter.
+         string v = null;
+         var t = v.ToString();
+      }
+   }
+}
