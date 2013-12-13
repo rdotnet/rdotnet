91e0e45e11653149185afb1fd4bf013b0492ee9c
J-M <jean-michel.perraud@csiro.au>
2013-12-13 11:49:17 +1100

    Bump version number as I use this for AWRA calibration and moving to nuget.

---
+++ R.NET/Properties/VersionInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+// DllVersion information for an assembly consists of the following four values:
+//
+//      Major DllVersion
+//      Minor DllVersion
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.5.6.0")]
+[assembly: AssemblyInformationalVersion("1.5.6")]
