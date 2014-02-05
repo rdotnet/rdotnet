5c0de343235754db06f437d52b141fbdfdab6007
J-M <jean-michel.perraud@csiro.au>
2014-02-06 07:03:25 +1100

    Add unit tests of issues reported by gchapman. Fix: IsList works as is.list.

---
+++ RDotNet.Tests/ListsTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace RDotNet
+{
+   public class ListsTest : RDotNetTestFixture
+   {
+      [Test]
+      public void TestIsList()
+      {
+         //https://rdotnet.codeplex.com/workitem/81         
+         var engine = REngine.GetInstanceFromID(EngineName);
+         var pairList = engine.Evaluate("pairlist(a=5)");
+         var aList = engine.Evaluate("list(a=5)");
+         bool b = aList.AsList().IsList();
+         Assert.AreEqual(true, pairList.IsList());
+         Assert.AreEqual(true, aList.IsList());
+      }
+
+      [Test]
+      public void TestListSubsetting()
+      {
+         //https://rdotnet.codeplex.com/workitem/81         
+         var engine = REngine.GetInstanceFromID(EngineName);
+         var numlist = engine.Evaluate("c(1.5, 2.5)").AsList();
+         var numListString = numlist.ToString();
+         var element = numlist[1];
+      }
+   }
+}
