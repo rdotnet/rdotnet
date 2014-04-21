9a93ef4d320fc6c56b11a3bd2ce75d08884256ea
J-M <jean-michel.perraud@csiro.au>
2014-04-21 17:29:40 +1000

    Work on runtime performance  and stress testing. Issue 109.

--- TestApps/MeasureRuntime/Program.cs
+++ TestApps/MeasureRuntime/Program.cs
@@ -18,38 +18,65 @@ namespace MeasureRuntime
          StartupParameter parameter = args.Length > 0 ?
             new StartupParameter() { MaxMemorySize = ulong.Parse(args[0]) * (1024 * 1024) } :
             new StartupParameter() { };
+         uint maxPowTwo = args.Length > 1 ? uint.Parse(args[1]) : 24;
 
-         using (var engine = REngine.GetInstance(device:device, parameter:parameter))
+         using (var engine = REngine.GetInstance(device: device, parameter: parameter))
          {
-             Console.WriteLine("************");
-             Console.WriteLine("*** NOTE: memory.limit() returns: " + engine.Evaluate("memory.limit()").AsNumeric()[0]);
-             Console.WriteLine("************");
-             //DoMeasuresVectors(engine);
-            DoMeasuresMatrices(engine);
+            printMemLimit(engine);
+            var measures = new List<Measurement>();
+
+            //engine.Evaluate("cases <- expand.grid(c(2,5,10), 10^(0:6))");
+            var sizes = engine.Evaluate(string.Format("as.integer(2^(0:{0}))", maxPowTwo)).AsInteger().ToArray();
+
+            RuntimeDiagnostics r = new RuntimeDiagnostics(engine)
+            {
+               Types = new[] { "numeric", "integer", "logical" }, // characters are much slower / cannot include here
+               Sizes = sizes
+            };
+
+            var funMap = RuntimeDiagnostics.GetMatrixCreationFunctions();
+            measures.AddRange(r.DoMeasures(funMap, what: "matrix", operation: "create", tag: ""));
+
+            funMap = RuntimeDiagnostics.GetVectorCreationFunctions();
+            measures.AddRange(r.DoMeasures(funMap, what: "vector", operation: "create", tag: ""));
+
+            funMap = RuntimeDiagnostics.GetMatrixRetrievalFunctions();
+            measures.AddRange(r.DoMeasures(funMap, what: "matrix", operation: "read", tag: ""));
+
+            funMap = RuntimeDiagnostics.GetVectorRetrievalFunctions();
+            measures.AddRange(r.DoMeasures(funMap, what: "vector", operation: "read", tag: ""));
+
+            engine.SetSymbol("runtimes", r.CollateResults(measures));
+            engine.Evaluate("write.table(runtimes, 'c:/tmp/runtimes.csv', row.names=FALSE, col.names=TRUE, quote=FALSE, sep = ',')");
+
             engine.Dispose();
          }
       }
 
-      private static void DoMeasuresMatrices(REngine engine)
+      private static void printMemLimit(REngine engine)
       {
-         RuntimeDiagnostics r = new RuntimeDiagnostics(engine);
+         Console.WriteLine("************");
+         Console.WriteLine("*** NOTE: memory.limit() returns: " + engine.Evaluate("memory.limit()").AsNumeric()[0]);
+         Console.WriteLine("************");
+      }
+
+      private static void DoMeasures(RuntimeDiagnostics r, IDictionary<string, Action<REngine, int, Stopwatch>> funMap,
+         string what, int[] sizes, string operation, string[] types, string tag)
+      {
+         var m = new List<Measurement>();
+
 #if DEBUG
          Console.WriteLine("NOTE Running code compiled in debug mode");
 #endif
-         Console.WriteLine();
-         Console.WriteLine("*** Faster matrices creation/initialisation ***");
-         Console.WriteLine();
+         //Console.WriteLine();
+         //Console.WriteLine("*** Faster matrices creation/initialisation ***");
+         //Console.WriteLine();
 
-         for (int n = 1; n < 2e7; n = n * 10)
+         foreach (var type in types)
          {
-            MeasureRuntimeOperation(r, RuntimeDiagnostics.CreateNumericMatrix, n, "numeric", "Create", "matrix");
-            MeasureRuntimeOperation(r, RuntimeDiagnostics.CreateIntegerMatrix, n, "integer", "Create", "matrix");
-            MeasureRuntimeOperation(r, RuntimeDiagnostics.CreateLogicalMatrix, n, "logical", "Create", "matrix");
+            for (int i = 0; i < sizes.Length; i++)
+               m.Add(MeasureRuntimeOperation(r, funMap[type], sizes[i], type, operation, what, tag));
          }
-
-         Console.WriteLine();
-         Console.WriteLine("*** Now keep going only with faster version of read operations ***");
-         Console.WriteLine();
 
          const int tenMillions = 10000000;
          // Cannot go past 1 billion elements, 
@@ -57,13 +84,13 @@ namespace MeasureRuntime
          // 1: Reached total allocation of 2047Mb: see help(memory.size)
          for (int n = 1; n < 11; n++)
          {
-            engine.ForceGarbageCollection();
-            MeasureRuntimeOperation(r, RuntimeDiagnostics.RtoDotNetNumericMatrix, n * tenMillions, "numeric", operation: "Read    ", what: "matrix");
+            //engine.ForceGarbageCollection();
+            MeasureRuntimeOperation(r, RuntimeDiagnostics.RtoDotNetNumericMatrix, n * tenMillions, "numeric", operation: "Read    ", what: what);
          }
          for (int n = 1; n < 11; n++)
          {
-            engine.ForceGarbageCollection();
-            MeasureRuntimeOperation(r, RuntimeDiagnostics.RtoDotNetIntegerMatrix, n * tenMillions, "integer", operation: "Read    ", what: "matrix");
+            //engine.ForceGarbageCollection();
+            MeasureRuntimeOperation(r, RuntimeDiagnostics.RtoDotNetIntegerMatrix, n * tenMillions, "integer", operation: "Read    ", what: what);
          }
       }
 
@@ -78,7 +105,7 @@ namespace MeasureRuntime
          Console.WriteLine("*** Faster vector creation/initialisation ***");
          Console.WriteLine();
 
-         for (int n = 1; n < 2e7; n = n*10)
+         for (int n = 1; n < 2e7; n = n * 10)
          {
             MeasureRuntimeOperation(r, RuntimeDiagnostics.CreateNumericVector, n, "numeric");
             MeasureRuntimeOperation(r, RuntimeDiagnostics.CreateIntegerVector, n, "integer");
@@ -91,7 +118,7 @@ namespace MeasureRuntime
          Console.WriteLine("*** Read R vector data into .NET Arrays: default and fast operation ***");
          Console.WriteLine();
 
-         for (int n = 1; n < 2e7; n = n*10)
+         for (int n = 1; n < 2e7; n = n * 10)
          {
             MeasureRuntimeOperation(r, RuntimeDiagnostics.RtoDotNetNumericVector, n, "numeric", operation: "Read    ");
          }
@@ -110,9 +137,11 @@ namespace MeasureRuntime
             MeasureRuntimeOperation(r, RuntimeDiagnostics.RtoDotNetIntegerVector, n * tenMillions, "integer", operation: "Read    ");
       }
 
-      private static void MeasureRuntimeOperation(RuntimeDiagnostics r, Action<REngine, int, Stopwatch> fun, int n, string type, string operation = "Create", string what = "vector")
+      private static Measurement MeasureRuntimeOperation(RuntimeDiagnostics r, Action<REngine, int, Stopwatch> fun, int n, string type, string operation = "Create", string what = "vector", string tag = "")
       {
-         Console.WriteLine(r.PrintRuntimeOperation(fun, n, type, operation, what));
+         var m = r.MeasureRuntimeOperation(fun, n, type, operation, what, tag);
+         Console.WriteLine(r.PrintRuntimeOperation(m));
+         return m;
       }
    }
 }
