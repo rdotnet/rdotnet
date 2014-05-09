076a6df0d5bde173f2a83221d79ba79a3c1f7d5d
J-M <jean-michel.perraud@csiro.au>
2014-05-09 15:36:00 +1000

    Make non-concurrent calls from multiple threads possible and more stable. Get ready for 1.5.12.

--- TestApps/StressTest/Program.cs
+++ TestApps/StressTest/Program.cs
@@ -24,7 +24,7 @@ namespace StressTest
             NumericVector[] nVecs = r.CreateNumericVectors(n, sizeEach);
             nVecs = r.Lapply(nVecs, "function(x) {x * 2}");
 
-            // doMultiThreadingOperation(r, sizeEach, n);
+            doMultiThreadingOperation(r, sizeEach, n, 50, false);
             doTestObjectFinalization(r, sizeEach, n);
             doTestObjectFinalization(r, sizeEach, n, disposeSexp:true);
             nVecs = null;
@@ -32,10 +32,20 @@ namespace StressTest
          }
       }
 
-      private static void doMultiThreadingOperation(RuntimeDiagnostics r, int sizeEach, int n)
+      private static void doMultiThreadingOperation(RuntimeDiagnostics r, int sizeEach, int n, int numOps, bool concurrent=false)
       {
-         var pVecs = new NumericVector[2][];
-         Parallel.For(0, 2, i => pVecs[i] = r.CreateNumericVectors(n, sizeEach));
+         var pVecs = new NumericVector[numOps][];
+         Parallel.For(0, numOps, i => CreateVector(r, sizeEach, n, pVecs, i, !concurrent));
+      }
+
+      private static Object lockedObj = new Object();
+
+      private static void CreateVector(RuntimeDiagnostics r, int sizeEach, int n, NumericVector[][] pVecs, int i, bool doLock)
+      {
+         if(doLock)
+            lock (lockedObj) { CreateVector(r, sizeEach, n, pVecs, i, false); }
+         else
+            pVecs[i] = r.CreateNumericVectors(n, sizeEach);
       }
 
       private static void doTestObjectFinalization(RuntimeDiagnostics r, int sizeEach, int n, bool disposeSexp=false)
