45d1c6a372df84e019611b28bc98182ab02cbf2d
MOKEYISH <mokeyish@hotmail.com>
2018-02-23 16:32:54 +0800

    add script for building *.Core

--- RDotNet.NativeLibrary/NativeUtility.cs
+++ RDotNet.NativeLibrary/NativeUtility.cs
@@ -3,8 +3,6 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
 using System.Text.RegularExpressions;
 
