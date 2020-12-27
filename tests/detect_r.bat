b7386bfc415632eab20d9649deac986c690c1624
Jean-Michel Perraud <per202@csiro.au>
2020-12-27 16:06:04 +1100

    Modify a test script to diagnose why appveyor VM returns non zero error code

--- tests/detect_r.bat
+++ tests/detect_r.bat
@@ -7,6 +7,7 @@ dotnet run --project TestApps/SimpleTest/SimpleTest.csproj
 @echo "WARN: errorlevel for SimpleTest is %errorlevel%, not 0"
 
 @IF '%errorlevel%'=='-1073740791' goto SimpleTestFailedBufOvrrun
+@IF '%errorlevel%'=='-1073740791' set exit_code=%errorlevel%
 
 :SimpleTestFailedBufOvrrun
 @echo "ERROR: exit code -1073740791 (0xC0000409). It means STATUS_STACK_BUFFER_OVERRUN. Shows up running netcoreapp3.1 with .NETCore on Appveyor infrastructure at 2020-12. Not reproducible on local PC."
