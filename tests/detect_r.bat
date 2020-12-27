f9e88f550c154438b2e2882cb764e1ba06d271b9
Jean-Michel Perraud <per202@csiro.au>
2020-12-27 16:28:46 +1100

    Add minimalist program to test failing test run on appveyor

--- tests/detect_r.bat
+++ tests/detect_r.bat
@@ -1,13 +1,27 @@
 @echo off
 set exit_code=0
 
-dotnet run --project TestApps/SimpleTest/SimpleTest.csproj 
+dotnet run --project minimal/minimal.csproj
 
 @if errorlevel 0 goto all_exit
 @echo "WARN: errorlevel for SimpleTest is %errorlevel%, not 0"
 
 @IF '%errorlevel%'=='-1073740791' set exit_code=%errorlevel%
+@IF '%errorlevel%'=='-1073740791' goto MinimalFailedBufOvrrun
+
+dotnet run --project TestApps/SimpleTest/SimpleTest.csproj 
+
+@echo "WARN: errorlevel for SimpleTest is %errorlevel%, not 0"
+
+@IF '%errorlevel%'=='-1073740791' set exit_code=%errorlevel%
 @IF '%errorlevel%'=='-1073740791' goto SimpleTestFailedBufOvrrun
+
+@if errorlevel 0 goto all_exit
+
+
+:MinimalFailedBufOvrrun
+@echo "ERROR: exit code -1073740791 (0xC0000409) on minimal.csproj. It means STATUS_STACK_BUFFER_OVERRUN. Shows up running netcoreapp3.1 with .NETCore on Appveyor infrastructure at 2020-12. Not reproducible on local PC."
+goto all_exit
 
 :SimpleTestFailedBufOvrrun
 @echo "ERROR: exit code -1073740791 (0xC0000409). It means STATUS_STACK_BUFFER_OVERRUN. Shows up running netcoreapp3.1 with .NETCore on Appveyor infrastructure at 2020-12. Not reproducible on local PC."
---
+++ tests/minimal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace minimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minimal program with System.Environment.Version => " + System.Environment.Version.ToString());
+            Console.WriteLine("System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription => " + System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.ToString());
+        }
+    }
+}
