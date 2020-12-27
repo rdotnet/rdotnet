b2ae6cb6525550f3fea833ceb92410c554527876
Jean-Michel Perraud <per202@csiro.au>
2020-12-27 16:12:27 +1100

    appveyor diagnosis

--- tests/detect_r.bat
+++ tests/detect_r.bat
@@ -6,8 +6,8 @@ dotnet run --project TestApps/SimpleTest/SimpleTest.csproj
 @if errorlevel 0 goto all_exit
 @echo "WARN: errorlevel for SimpleTest is %errorlevel%, not 0"
 
-@IF '%errorlevel%'=='-1073740791' goto SimpleTestFailedBufOvrrun
 @IF '%errorlevel%'=='-1073740791' set exit_code=%errorlevel%
+@IF '%errorlevel%'=='-1073740791' goto SimpleTestFailedBufOvrrun
 
 :SimpleTestFailedBufOvrrun
 @echo "ERROR: exit code -1073740791 (0xC0000409). It means STATUS_STACK_BUFFER_OVERRUN. Shows up running netcoreapp3.1 with .NETCore on Appveyor infrastructure at 2020-12. Not reproducible on local PC."
